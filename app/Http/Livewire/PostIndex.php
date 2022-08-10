a7ca2248a1d07b2d4c3cfdfa561f8b379bc09fa4
lokeshkumar12471 <105625414+lokeshkumar12471@users.noreply.github.com>
2022-08-10 16:46:14 +0530

    storing the Data

--- app/Http/Livewire/PostIndex.php
+++ app/Http/Livewire/PostIndex.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class PostIndex extends Component
 {
+    use WithFileUploads;
+    public $showingPostModal = false;
+    public $newImage;
+    public $body;
+    public $title;
+    public function showPostModal()
+    {
+        $this->showingPostModal = true;
+    }
+    public function storePost()
+    {$this->validate([
+        'title' => 'required',
+        'body' => 'required',
+        'newImage' => 'image',
+
+    ]);
+
+        $image = $this->newImage->store('public/posts');
+        Post::create([
+            'title' => $this->title,
+            'image' => $image,
+            'body' => $this->body,
+        ]);
+        $this->reset();
+
+    }
     public function render()
     {
-        return view('livewire.post-index');
+        return view('livewire.post-index', ['posts' => Post::all()]);
     }
 }
